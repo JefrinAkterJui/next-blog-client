7f2b182acea5d8b9d661dbc0fdaa0dec34a9f886
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-09-29 09:19:10 +0600

    Show off all the fantastic blogs using SSR

--- src/app/(public)/blogs/page.tsx
+++ src/app/(public)/blogs/page.tsx
@@ -1,7 +1,22 @@
-const AllBlogsPage = () => {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import BlogCard from "@/components/modules/Blogs/BlogCard";
+
+const AllBlogsPage = async() => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`,{
+    cache:"no-store"
+  })
+  const json = await res.json()
+  const blogs = json?.data?.data || [];
   return (
     <div className="py-30 px-4 max-w-7xl mx-auto">
       <h2 className="text-center text-4xl">All Blogs</h2>
+      <div className="grid grid-cols-3 gap-4 mx-auto min-h-6 my-5">
+        {
+          blogs.map((blog:any)=>(
+            <BlogCard key={blog.id} post={blog}/>
+          ))
+        }
+      </div>
     </div>
   );
 };
